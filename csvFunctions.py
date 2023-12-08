07dde91c4bcdf7803365d04b21a6a8affa7c9b70
tdymo <151123541+tdymo@users.noreply.github.com>
2023-12-07 20:48:45 -0600

    Added Unique Voting interface, Docstrings, and Type hinting.

---
+++ csvFunctions.py
@@ -0,0 +1,102 @@
+import csv
+from typing import Tuple
+
+def can_vote(name: str) -> bool:
+    """
+    This function should determine if someone can vote.
+        
+    The function takes in the name of a person and reads through a csv file to determine if they are on the list of eligible voters.
+    """
+    with open('NamesWhoCanVote.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for value in row:
+                if name in value:
+                    return True
+    return False
+def has_voted(name: str) -> bool:
+    """
+    This function should determine if someone has already voted.
+        
+    The function takes in the name of a person and reads through a csv file to determine if they have already voted.
+    """
+    with open('NamesWhoHaveVoted.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for value in row:
+                if name in value:
+                    return True
+    return False
+
+def allowed_to_vote(name: str) -> bool:
+    """
+    This function should determine if someone is allowed to vote.
+        
+    This function takes in the name of a person and then runs the name
+    through the can_vote and has_voted functions.
+    
+    If the can_vote function returns True and the has_voted function returns false,
+    the allowed_to_vote function will return True.
+    """
+    if can_vote(name) == True:
+        if has_voted(name) == False:
+            return True
+    return False
+
+def just_voted(name: str) -> None:
+    """
+    This function should update NamesWhoHaveVoted.csv with the name given in the input.
+    
+    This function takes in a name as input and adds the name to the csv file.
+    It returns nothing.
+    """
+    with open('NamesWhoHaveVoted.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name])
+        
+        
+def jane_voted() -> None:
+    """
+    This function should update Votes.csv with the name 'jane'.
+    
+    This function takes no input and returns nothing.
+    """
+    
+    with open('Votes.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["jane"])
+
+
+def john_voted() -> None:
+    """
+    This function should update Votes.csv with the name 'john'.
+    
+    This function takes no input and returns nothing.
+    """
+    with open('Votes.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["john"])
+        
+def count_votes() -> Tuple(int, int):
+    """
+    This function should return a tuple containing the number
+    of votes john has and the number of votes jane has.
+    
+    The function reads through the Votes.csv and counts the votes
+    for jane and the votes for john
+    
+    This function takes no input and returns a tuple with 2 elements.
+    """
+    john_votes = 0
+    jane_votes = 0
+    with open('Votes.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for value in row:
+                if "jane" in value:
+                    jane_votes += 1
+                if "john" in value:
+                    john_votes += 1
+    return john_votes, jane_votes
+        
+        
--- voteGUI.py
+++ voteGUI.py
@@ -1,11 +1,16 @@
 import tkinter
+from csvFunctions import *
+
 
 class voteGUI:
-    global john_votes
-    global jane_votes
-    john_votes = 0
-    jane_votes = 0
+    
     def __init__(self, window):
+        """
+        Initializes the GUI by creating the main window.
+        User should have the choice to either vote or view current results.
+        """
+        
+        
         self.window = window
     
         self.header_label = tkinter.Label(window, text = "What would you like to do?")
@@ -17,26 +22,81 @@ class voteGUI:
         self.Results_Button.pack()
         
     def vb_clicked(self):
-        self.header_label.config(text = "Who would you like to vote for?")
+        """
+        This function should be called when Vote_Button is pressed.
+        
+        The window should change and create an entry box for the user to enter their name.
+        A submit button is created beneath this entry box.
+        """
+        self.header_label.config(text = "Please Enter Your First and Last Name")
         self.Vote_Button.pack_forget()
         self.Results_Button.pack_forget()
         
-        self.John_Button = tkinter.Button(self.window, text = "John", command = self.john_clicked, width = 10, height = 5, bg = "green")
-        self.John_Button.pack()
-        self.Jane_Button = tkinter.Button(self.window, text = "Jane", command = self.jane_clicked, width = 10, height = 5, bg = "orange")
-        self.Jane_Button.pack()
+        self.Name_Entry = tkinter.Entry(self.window, width = 30)
+        self.Name_Entry.pack()
+        self.Submit_Name_Button = tkinter.Button(self.window, text = "Submit", command = self.submit_clicked, width = 10, height = 1,)
+        self.Submit_Name_Button.pack()
         
+        
+    
+    def submit_clicked(self):
+        """
+        This function should be called when Submit_Name_Button is pressed.
+        
+        Upon being called, the entry in the box should be stored.
+        If this value is empty, the message "Please Enter Name"
+        
+        If the name is among those who can vote and have not already voted, they will be taken to the vote screen
+        If they cannot vote, an error message is displayed.
+        """
+        name = self.Name_Entry.get()
+        if name == "":
+            try:
+                self.error_label.config(text = "Please Enter Name.")
+            except AttributeError:
+                self.error_label = tkinter.Label(self.window, text = "Please Enter Name.")
+                self.error_label.pack()
+        elif allowed_to_vote(name) == True:
+            just_voted(self.Name_Entry.get())
+            self.header_label.config(text = "Who would you like to vote for?")
+            self.Name_Entry.pack_forget()
+            self.Submit_Name_Button.pack_forget()
+            
+            try:
+                self.error_label.pack_forget()
+            except:
+                pass
+            
+            self.John_Button = tkinter.Button(self.window, text = "John", command = self.john_clicked, width = 10, height = 5, bg = "green")
+            self.John_Button.pack()
+            self.Jane_Button = tkinter.Button(self.window, text = "Jane", command = self.jane_clicked, width = 10, height = 5, bg = "orange")
+            self.Jane_Button.pack()
+            
+        else:
+            try:
+                self.error_label.config(text = "Name Ineligible To Vote. Check Spelling and Capitalizaton.")
+            except AttributeError:
+                self.error_label = tkinter.Label(self.window, text = "Name Ineligible To Vote. Check Spelling and Capitalizaton.")
+                self.error_label.pack()
+    
     def rb_clicked(self):
+        """
+        This function should be called when Results_Button is pressed.
+        
+        Upon being called, all buttons should be removed and the vote count for both John and Jane should be displayed
+        """
         self.header_label.config(text = "Current Voting Results")
         self.Vote_Button.pack_forget()
         self.Results_Button.pack_forget()
         
+        john_votes = count_votes()[0]
+        jane_votes = count_votes()[1]
         if john_votes == 1:
-            self.john_label = tkinter.Label(self.window, text = f"John has 1 vote")
+            self.john_label = tkinter.Label(self.window, text = "John has 1 vote")
         else:
             self.john_label = tkinter.Label(self.window, text = f"John has {john_votes} votes")
         if jane_votes == 1:
-            self.jane_label = tkinter.Label(self.window, text = f"Jane has 1 vote")
+            self.jane_label = tkinter.Label(self.window, text = "Jane has 1 vote")
         else:
             self.jane_label = tkinter.Label(self.window, text = f"Jane has {jane_votes} votes")
         self.john_label.pack()
@@ -44,10 +104,15 @@ class voteGUI:
         
         
     def john_clicked(self):
-        global john_votes
-        john_votes += 1
+        """
+        This function should be called when John_Button is pressed.
         
-        self.header_label.config(text = "You voted for John. What would you like to do next?")
+        Upon being called, the user should be returned to the orignial screen with the thank you for voting message.
+        """
+        
+        john_voted()
+        
+        self.header_label.config(text = "Thank you for voting. What would you like to do next?")
         self.header_label.pack()
 
         self.John_Button.pack_forget()
@@ -61,10 +126,14 @@ class voteGUI:
         
         
     def jane_clicked(self):
-        global jane_votes
-        jane_votes += 1
+        """
+        This function should be called when Jane_Button is pressed.
         
-        self.header_label.config(text = "You voted for Jane. What would you like to do next?")
+        Upon being called, the user should be returned to the orignial screen with the thank you for voting message.
+        """
+        jane_voted()
+        
+        self.header_label.config(text = "Thank you for voting. What would you like to do next?")
         self.header_label.pack()
         
         self.John_Button.pack_forget()
